e135304e9b09a1f95418b841398a3ac4e56e9692
Veicm <connormannes@gmx.de>
2025-06-01 20:14:38 +0200

    update 2

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name='dev_ink',
     version='0.9.0',
     packages=find_packages(),
-    include_package_data=True,
     entry_points={
         'gui_scripts': [
             'dev_ink = dev_ink.__main__:main'
